0e51ff43fc0297b3ecc00141569bd30054eceb9c
Brandon Helt <brandon.helt@yahoo.com>
2022-05-06 15:24:32 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTask(document.getElementById('new-task-description').value);
+  })
+
+  function addTask(task) {
+    const list = document.getElementById('tasks');
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
+    const button = document.createElement('button');
+    button.textContent = 'x';
+    button.addEventListener('click', deleteTask);
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+  }
+
+  function deleteTask(e) {
+    e.target.parentNode.remove();
+  }
 });
